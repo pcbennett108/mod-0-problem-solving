ff5a132520f14d828d56ab4bbbc54ac926bb16df
pcbennett108 <pcbennett42@gmail.com>
2023-04-14 13:22:36 -0600

    Change challenge5.rb quote to only lowercase letters

--- challenge5.rb
+++ challenge5.rb
@@ -4,7 +4,7 @@
 # maybe there is a built in Ruby method that does this... gsub
 
 # Final Solution
-quote = "three quarks for muster Mark!"
+quote = "three quarks for muster mark!"
 
 # Here I use gsub to globally substitute each word ( the regex \w+ gives us each word)
 # Then in the block, gsub is subbing each word with that word capitalized.
