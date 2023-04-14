cc4511c66d8701ad98b2dbc965b4d4fdb169bce0
pcbennett108 <pcbennett42@gmail.com>
2023-04-14 13:20:28 -0600

    Add conpleted challenge5.rb

---
+++ challenge5.rb
@@ -0,0 +1,12 @@
+# Take a string sentance and print it to the console with the first letter of every word capitalized.
+
+# loop through the input string and CAP every word...
+# maybe there is a built in Ruby method that does this... gsub
+
+# Final Solution
+quote = "three quarks for muster Mark!"
+
+# Here I use gsub to globally substitute each word ( the regex \w+ gives us each word)
+# Then in the block, gsub is subbing each word with that word capitalized.
+quote = quote.gsub(/\w+/) { |word| word.capitalize }
+p quote
